702accae1947fda7d1faff3b3317364edcfd1a07
Melis Yazıcı <melis.yazici95@icloud.com>
2022-10-17 19:42:19 +0200

    Add hi.c

---
+++ hi.c
@@ -0,0 +1,9 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string s = "HI! BYE!";
+
+    printf("%i %i %i %i %i %i %i %i %i\n", s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8]);
+}
