33e34dada530e0b8c2ae27a5be46e36e6f0226f3
Melis Yazıcı <melis.yazici95@icloud.com>
2022-10-17 19:31:58 +0200

    Add compare.c

---
+++ compare.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    // int i = get_int("i: ");
+    // int j = get_int("j: ");
+
+    // if (i == j) {
+    //     printf("Same\n");
+    // }
+    // else {
+    //     printf("Different\n");
+    // }
+
+char *s = get_string("s: ");
+char *t = get_string("t: ");
+
+// if (strcmp(s, t) == 0) {
+//     printf("Same\n");
+// }
+// else {
+//     printf("Different\n");
+// }
+
+printf("%p\n", s);
+printf("%p\n", t);
+
+
+}
